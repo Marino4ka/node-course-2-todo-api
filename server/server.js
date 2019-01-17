f209f9cbbe3e92646988b4ce1aab2ad06daf9812
Marina <marina.iovbak@gmail.com>
2019-01-17 14:57:44 +0600

    Added tests cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -44,11 +44,11 @@ app.get("/todos/:id", (req, res) => {
 		if(!todo) {
 			return res.status(404).send();
 		}
+
 		res.send({todo});
 	}).catch((e) => {
 		res.status(400).send();
 	});
-
 });
 		 	 
 		 // error
