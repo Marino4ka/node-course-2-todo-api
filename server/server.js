b7aea6d8e292c309cb5da5e1128318b381e7365b
Marina <marina.iovbak@gmail.com>
2019-01-18 13:50:47 +0600

    Added Delete /todos/:id route

--- server/server.js
+++ server/server.js
@@ -56,6 +56,24 @@ app.get("/todos/:id", (req, res) => {
 		 	// 400 - and send empty body back 
 
 
+app.delete("/todos/:id", (req, res) => {
+	var id = req.params.id;
+
+	if(!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	};
+
+	Todo.findByIdAndRemove(id).then((todo) => {
+		if(!todo) {
+			return res.status(404).send();
+		}
+		res.send({todo});
+
+	}).catch((e) => {
+		res.send(400).send();
+	});
+});
+
 app.listen(port, () => {
 	console.log(`Started up at ${port}`);
 });
